325e557ba41a6cb5c4f65f4434fc625c3b3df5b4
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-02-04 17:26:15 -0800

    Add footer links Wazoo and FartLabs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,23 +5,25 @@ export function Footer() {
     <footer class="w-11/12 max-w-5xl mx-auto mt-24 sm:!mt-28 mb-8 flex items-center justify-between">
       <span class="flex items-center gap-4">
         <a
-          class="flex items-center gap-2 text-gray-700"
-          href="https://fresh.deno.dev"
-        >
-          <a href="https://fresh.deno.dev">
-            <img
-              width="197"
-              height="37"
-              src="https://fresh.deno.dev/fresh-badge.svg"
-            />
-          </a>
-        </a>
-        <a
           class="hover:underline flex gap-2 items-center"
           href="https://github.com/FartLabs/shop"
         >
           <GitHub class="h-5 w-5 text-gray-500" />
           Source
+        </a>
+
+        <a
+          class="hover:underline flex gap-2 items-center"
+          href="https://wazoo.tech"
+        >
+          ⭐ Wazoo
+        </a>
+
+        <a
+          class="hover:underline flex gap-2 items-center"
+          href="https://fartlabs.org"
+        >
+          🧪 FartLabs
         </a>
       </span>
       <span class="text-sm text-gray-400 hidden items-center gap-2 sm:!flex">
